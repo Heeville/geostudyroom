443d2778d95e303142990777762768d4d1b96ec1
홍진욱 <jinuk@hongjin-ug-ui-MacBookAir.local>
2023-09-22 16:44:59 +0900

    타임 테이블 수정

--- reserve/serializers.py
+++ reserve/serializers.py
@@ -4,9 +4,20 @@ from geouser.models import *
 from geouser.serializers import *
 
 class StudyroomSerializer(serializers.ModelSerializer):
+    time = serializers.SerializerMethodField()
     class Meta:
         model=StudyRoom
-        exclude=['rclock']
+        fields=['id','name','date','time']
+        
+    # "time_fields" 필드의 값을 설정하는 메서드를 정의합니다.
+    def get_time(self, obj):
+        time_fields = {}
+        for field_name in obj._meta.get_fields():
+            if field_name.name.startswith('time'):
+                # "time"으로 시작하는 필드 이름에서 "time" 부분을 제거하고 사용합니다.
+                short_field_name = field_name.name[4:6]+":"+ field_name.name[6:]
+                time_fields[short_field_name] = getattr(obj, field_name.name)
+        return time_fields
         
 class ReservationSerializer(serializers.ModelSerializer):
     #schoolnumber=serializers.SerializerMethodField()
